34122c5489a46afa7cc136e0e2a479f2b6510835
TanKittipat <kittipat12tan@gmail.com>
2024-08-29 16:29:23 +0700

    1st

---
+++ src/TemplateMethodDemo.java
@@ -0,0 +1,10 @@
+
+public class TemplateMethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Generalization algorithm = new Realization();
+        algorithm.findSolution();
+	}
+
+}
